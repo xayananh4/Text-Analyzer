9a17e89298efde0126c54783e86ca9596fc7a98b
Stephen <szook7@gmail.com>
2023-01-17 14:37:56 -0800

    completed first part of TDD

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Business Logic
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;}
+  });
+  return wordCount;
+}
